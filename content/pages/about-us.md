00d311e6492dd4fd275dfee0247f0a01052193d8
Animated-Eagle <sheddinghorribly@gmail.com>
2019-03-17 18:14:57 -0400

    about us page

---
+++ content/pages/about-us.md
@@ -0,0 +1,10 @@
+---
+type: page
+layout: about-us
+title: 'ABOUT ON POINT CUSTOM HOMES'
+url: /about-us
+params:
+page-status: 'about-us'
+pageImage: '/v1552694734/OnPoint%20Custom%20Homes/14-1400x788.jpg'
+pageTitle: 'ABOUT ON POINT CUSTOM HOMES'
+---
--- themes/site-theme/src/css-config.js
+++ themes/site-theme/src/css-config.js
@@ -52,6 +52,7 @@ let colors = {
   'color-e1e9ba': '#e1e9ba',
   'color-48525d': '#48525d',
   'color-818e9c': '#818e9c',
+  'color-eeeeee': '#eeeeee',
   'color-header': "rgba(70, 74, 78, 0.95)",
   'color-999999': '#999999',
   'transparent': 'transparent',
